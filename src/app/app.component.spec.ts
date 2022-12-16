990271eba2886242e5384a283eb13a186b820604
Jasmin Balochiya <jasmin.balochiya@gmail.com>
2022-12-16 17:29:21 +0530

    App created.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MyBlog'`, () => {
+  it(`should have as title 'Blog'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MyBlog');
+    expect(app.title).toEqual('Blog');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('MyBlog app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Blog app is running!');
   });
 });
